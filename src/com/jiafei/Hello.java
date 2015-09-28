d82c0005ac3d62b5ca28ec61ee56ea6c98177e6f
yangjiabin <892986030@qq.com>
2015-09-28 16:56:06 +0800

    201509281656

--- src/com/jiafei/Hello.java
+++ src/com/jiafei/Hello.java
@@ -11,6 +11,19 @@ public class Hello implements Runnable{
 	}
 	
 	static class World{
-		
+		private String guid;
+		private String name;
+		public String getGuid() {
+			return guid;
+		}
+		public void setGuid(String guid) {
+			this.guid = guid;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
 	}
 }
