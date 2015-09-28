3c44c516a056ebbfb3c2d0435002250ba0dac18b
yangjiabin <892986030@qq.com>
2015-09-28 16:50:34 +0800

    201509281650

--- src/com/jiafei/Hello.java
+++ src/com/jiafei/Hello.java
@@ -1,7 +1,12 @@
 package com.jiafei;
 
-public class Hello {
+public class Hello implements Runnable{
 	public static void main(String[] args) {
 		System.out.println(".............");
+	}
+
+	@Override
+	public void run() {
+		System.out.println("run");
 	}
 }
